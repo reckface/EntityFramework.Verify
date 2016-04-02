d3a25f274f062f4b6b5d8b35d26912ce22a8c127
reckface <reckface@users.noreply.github.com>
2016-04-02 22:44:08 +0100

    Update AssemblyInfo.cs

--- EntityFramework.Verify/Properties/AssemblyInfo.cs
+++ EntityFramework.Verify/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EntityFramework.Verify")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generic library to verify an entity model against a production database")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("pronounced80")]
 [assembly: AssemblyProduct("EntityFramework.Verify")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
